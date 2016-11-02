569265049ff525fe58956aa6319eea35d51848e3
ana0 <sarah.anne.friend@gmail.com>
2016-11-02 15:19:32 -0400

    Start/Stop button, drawing a continuous line

--- assets/js/sigilpro.js
+++ assets/js/sigilpro.js
@@ -6,7 +6,12 @@ var g = [0,1]
 var b = 255
 var a = 0.8
 
-var x, y
+var x, y;
+var oldX, oldY;
+var currentlyDrawing = false;
+var currentlyCasting = false;
+var theta = 0;
+var startCastTime = 0.0;
 
 var matchMedia = window.msMatchMedia || window.MozMatchMedia || window.WebkitMatchMedia || window.matchMedia
 
@@ -15,59 +20,117 @@ function setup(){
 	canvas.parent(main)
 	fill("#55f")
 	stroke("#55f")
+    strokeWeight(2)
 	x = main.offsetWidth/2
 	y = main.offsetHeight/2
+    oldX = x;
+    oldY = y;
 
     castButton = createButton('Cast');
     castButton.addClass('cssCastButton');
+    castButton.mousePressed(triggerCastingAnimation);
     discardButton = createButton('Discard');
     discardButton.addClass('cssDiscardButton');
     discardButton.mousePressed(discardDrawing);
+    stopButton = createButton('Stop');
+    stopButton.addClass('cssStopButton');
+    stopButton.mouseClicked(stopDrawing);
+    startButton = createButton('Start');
+    startButton.addClass('cssStartButton');
+    startButton.mouseClicked(startDrawing);
+    stopButton.hide();
 }
 
 function draw(){
 	wrapColor(g)
 	fill(r, g[0], b)
-	stroke(r, g[0], b, a)
+	stroke(r, g[0], b)
 
 	if(window.DeviceMotionEvent){
 		window.addEventListener("devicemotion", followMotion, false)
 	}else{
 		console.log("DeviceMotionEvent is not supported")
 	}
+
+    if (currentlyCasting) {
+        castSigil();
+        if (millis() - startCastTime > 10000) {
+            discardDrawing();
+            currentlyCasting = false;
+        }
+    }
 }
 
 function followMotion(e){
+    if (currentlyDrawing) {
+    	// var mx = main.offsetWidth/2
+    	// var my = main.offsetHeight/2
 
-	// var mx = main.offsetWidth/2
-	// var my = main.offsetHeight/2
+    	// ellipse(mx, my, 5, 5)
+        oldX = x
+        oldY = y
 
-	// ellipse(mx, my, 5, 5)
+    	x -= e.accelerationIncludingGravity.x
+    	if (x < 0){
+    		x = 0
+    	}
+    	else if(x > main.offsetWidth){
+    		x = main.offsetWidth
+    	}
+    	// x = constrain(x, [0, main.offsetWidth])
+    	
+    	y += e.accelerationIncludingGravity.y
+    	if (y < 0){
+    		y = 0
+    	}
+    	else if(y > main.offsetHeight){
+    		y = main.offsetHeight
+    	}
 
-	x -= e.accelerationIncludingGravity.x
-	if (x < 0){
-		x = 0
-	}
-	else if(x > main.offsetWidth){
-		x = main.offsetWidth
-	}
-	// x = constrain(x, [0, main.offsetWidth])
-	
-	y += e.accelerationIncludingGravity.y
-	if (y < 0){
-		y = 0
-	}
-	else if(y > main.offsetHeight){
-		y = main.offsetHeight
-	}
-
-	ellipse(x, y, 5, 5)
-	
+    	//ellipse(x, y, 5, 5)
+        line(oldX, oldY, x, y);
+    } 
 }
 function discardDrawing() {
     x = main.offsetWidth/2
     y = main.offsetHeight/2
     background(0,0,0);
+    stopButton.hide();
+    startButton.show();
+    currentlyDrawing = false;
+}
+
+function triggerCastingAnimation() {
+    startCastTime = millis();
+    currentlyCasting = true;
+    currentlyDrawing = false;
+    theta = 0.0;
+}
+
+function castSigil() {
+    circleXsm = 50 * cos(theta) + (windowWidth/2);
+    circleYsm = 50 * sin(theta) + (windowHeight/2);
+    ellipse(noise(circleXsm), noise(circleYsm), 5, 5);
+    circleX = 100 * cos(theta) + (windowWidth/2);
+    circleY = 100 * sin(theta) + (windowHeight/2);
+    ellipse(circleX, circleY, 5, 5);
+    circleXlg = 200 * cos(theta) + (windowWidth/2);
+    circleYlg = 200 * sin(theta) + (windowHeight/2);
+    ellipse(circleXlg, circleYlg, 5, 5);
+    theta += 1;
+    background('rgba(0%,0%,0%,0.01)')
+}
+
+function startDrawing() {
+    currentlyDrawing = true;
+    startButton.hide();
+    stopButton.show();
+}
+
+function stopDrawing() {
+    currentlyDrawing = false;
+    stopButton.hide();
+    startButton.show();
 }
 
 /** convert a value from one range to another ***/
